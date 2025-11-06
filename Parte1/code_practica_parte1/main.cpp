1a3917aea55fcb08dafb81fd13d10b667bb075aa
Jhon Tristancho <jhon.tristancho@udea.edu.co>
2025-11-06 15:46:42 -0500

    repositorio listo para empezar a trabajar

---
+++ Parte1/code_practica_parte1/main.cpp
@@ -0,0 +1,10 @@
+//tratemos de que si van a hacer pruebas en el main, borrar antes de hacer el push. para que no se sature.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
