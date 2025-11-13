32d865a93390ed30c9259683823dab3685c2aaf2
Alexa4 <alexa.carolina4497@gmail.com>
2025-11-12 21:28:43 -0500

    cambios en el main.cpp

--- Parte1/code_practica_parte1/main.cpp
+++ Parte1/code_practica_parte1/main.cpp
@@ -1,10 +1,28 @@
-//tratemos de que si van a hacer pruebas en el main, borrar antes de hacer el push. para que no se sature.
-#include <iostream>
+#include "Simulador.h"
 
-using namespace std;
+int main() {
+    //  Caja un poco más pequeña para que haya más choques visibles
+    Simulador sim(500, 350, 0.6); // coef. restitución 0.6
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    // --- Partículas bien espaciadas (trayectorias parabólicas limpias) ---
+    Particula p1(80, 300, 70, -120, 2, 10);   p1.gravedad = 9.0;
+    Particula p2(150, 250, 60, -140, 3, 10);  p2.gravedad = 9.0;
+    Particula p3(300, 280, -70, -100, 4, 12); p3.gravedad = 9.0;
+    Particula p4(420, 260, -90, -110, 2, 10); p4.gravedad = 9.0;
+
+    sim.agregarParticula(p1);
+    sim.agregarParticula(p2);
+    sim.agregarParticula(p3);
+    sim.agregarParticula(p4);
+
+    // --- Obstáculos centrados para que las trayectorias los usen ---
+    sim.agregarObstaculo(Obstaculo(200, 120, 50));  // Centro izquierda
+    sim.agregarObstaculo(Obstaculo(320, 100, 50));  // Centro derecha
+    sim.agregarObstaculo(Obstaculo(260, 200, 40));  // Bien centrado
+    sim.agregarObstaculo(Obstaculo(150, 150, 40));  // Para interacción de P2
+
+    // Correr simulación
+    sim.simular(10, 0.05);
+
     return 0;
 }
